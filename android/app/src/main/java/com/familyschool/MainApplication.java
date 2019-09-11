d45ea99c468ebec203915a65dab97eb0b29fb9ba
Chase <939739229@qq.com>
2019-09-12 00:37:22 +0800

    andorid 聊天ui

--- android/app/src/main/java/com/familyschool/MainApplication.java
+++ android/app/src/main/java/com/familyschool/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import cn.jiguang.imui.messagelist.ReactIMUIPackage; //聊天UI
 
 import java.util.List;
 
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
+      packages.add(new ReactIMUIPackage());
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       return packages;
